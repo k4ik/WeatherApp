bdde2f66d9cc8879eb35b810a782d7bdad8f1214
Kaik <kaikleal1234@gmail.com>
2023-11-15 14:51:56 -0300

    api

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,17 +5,28 @@ async function fetchWeatherData(locationParam) {
         let locationName = document.querySelector(".locationName");
         let temp = document.querySelector(".temp");
 
-        locationName.innerHTML = `${weatherData.location.name}`;
-        temp.innerHTML = `${weatherData.current.temp_c}°C`;
+        locationName.innerHTML = 
+        `<img src="assets/images/map-pin.svg" alt="">
+        <span>${weatherData.location.name}</span>`;
+
+        temp.innerHTML = 
+        `<img src="assets/images/thermometer.svg" alt="">
+        <span>${weatherData.current.temp_c}°C</span>`;
 
         console.log(weatherData);
+
+        locationName.style.display = "flex";
+        temp.style.display = "flex";
+
         return weatherData;
     } catch(e) {
         console.log(e);
+        alert("location not found");
     }
 }
 
 let locationInput = document.querySelector("#locationInput");
+locationInput.value = "";
 
 let weatherForm = document.querySelector("#weatherForm");
 
@@ -23,4 +34,4 @@ weatherForm.addEventListener('submit', function(event) {
     event.preventDefault();
 
     fetchWeatherData(locationInput.value);
-});
+}); 
