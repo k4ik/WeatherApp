823c4859ed07639cae6d58787e2f755a6b6648f7
k4ik <kaikleal1234@gmail.com>
2023-12-30 10:53:31 -0300

    :sparkles: feat: App Component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,48 @@
 <script>
-export default {
-  name: 'App'
-}
+  import Card from "./components/Card/Card.vue"
+  import Title from "./components/Title/Title.vue"
+  import Message from "./components/Message/Message.vue"
+
+  export default {
+    name: 'App',
+    components: {
+      Title,
+      Card,
+      Message
+    }
+  }
 </script>
 
 <template>
-  <main></main>
+  <main class="main">
+    <Title />
+    <Card />
+  </main>
 </template>
 
-<style></style>
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap');
+
+  body {
+    background: url("../public/images/bg.jpg");
+    background-position: top;
+    background-size: cover;
+    background-repeat: no-repeat;
+    height: 100vh;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Poppins', sans-serif;
+  }
+
+  .main {
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
