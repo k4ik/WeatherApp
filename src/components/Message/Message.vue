ec590b6e31e5327aa9bc8f702ac914cd6e9343b3
k4ik <kaikleal1234@gmail.com>
2023-12-30 10:52:56 -0300

    :sparkles: feat: Message Component

---
+++ src/components/Message/Message.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <p>Location not found. Try again!</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Message"
+    }
+</script>
+
+<style scoped src="./Message.scss" lang="scss" />
