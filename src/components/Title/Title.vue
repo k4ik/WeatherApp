4096ba4349699dcaf867bb12fd746ec3b590cbcf
k4ik <kaikleal1234@gmail.com>
2023-12-30 10:53:13 -0300

    :sparkles: feat: Title Component

---
+++ src/components/Title/Title.vue
@@ -0,0 +1,13 @@
+<template>
+    <main>
+        <h1>Weather app</h1>
+    </main>
+</template>
+
+<script>
+    export default {
+        name: 'Title'
+    }
+</script>
+
+<style lang="scss" src="./Title.scss" scoped />
